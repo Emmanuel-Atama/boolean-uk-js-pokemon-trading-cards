7b5c7fdeee39a84d01b180a9ae2d2e154efdd42a
Emmanuel Atama <emmanuelatama@outlook.com>
2021-07-25 15:29:04 +0100

    Completed

--- src/index.js
+++ src/index.js
@@ -34,38 +34,102 @@
 //   - append to the element from Step 1.0
 // - input: an array of objects that represent pokemon (data)
 // - output: -
-const ulEl = document.querySelector("ul")
 
-function renderPokemon (data) {
-   let pokemonCard = {}
-   for (let i = 0; i < data.length; i++) {
-const item = data[i]
-// console.log("Pokemon Data: ", item)
 
-const name = item.name
-// console.log("Pokemon Names: ", name)
-const image = item.sprites.other["official-artwork"].front_default
-// console.log("Pokemon Images: ", image)
-const titleEl = document.createElement("h2")
-document.body.append(titleEl)
-titleEl.innerText = name
-//Create Image Source
-const imgEl = document.createElement("img")
-document.body.append(imgEl)
-imgEl.setAttribute("src", image)
-   }
-   return pokemonCard
+// function renderPokemon (pokemon) {
+//    let pokemonCard = {}
+//    for (let i = 0; i < data.length; i++) {
+// const item = data[i]
+// // console.log("Pokemon Data: ", item)
+// const name = item.name
+// // console.log("Pokemon Names: ", name)
+// const image = item.sprites.other["official-artwork"].front_default
+// // console.log("Pokemon Images: ", image)
+// const titleEl = document.createElement("h2")
+// document.body.append(titleEl)
+// titleEl.innerText = name
+// //Create Image Source
+// const imgEl = document.createElement("img")
+// document.body.append(imgEl)
+// imgEl.setAttribute("src", image)
+//    }
+//    return pokemonCard
+// }
+// const pokemonCards = renderPokemon(data)
+// // console.log("pokemonCards: ", pokemonCards)
+
+
+// // const statistics = data.stats
+// function renderStatsList(data) {
+//   let pokemonStatistics = {};
+//   for (let i = 0; i < data.length; i++) {
+//     const item = data[i];
+//     // console.log("Pokemon Data:", item);
+//     const pokemonStats = item.stats[i];
+//     // console.log("Pokemon Stats: ", pokemonStats);
+//     const base = pokemonStats.base_stat;
+//     console.log("Base: ", base);
+//   }
+//   return pokemonStatistics;
+// }
+// const pokemonRenderStats = renderStatsList(data);
+// // console.log("pokemonRenderStats: ", pokemonRenderStats)
+
+// console.log(data)
+
+const listEl = document.querySelector(".cards")
+
+// console.log("The Bridge: ", listEl)
+
+function renderPokemonCard(pokemon) {
+  // console.log("Inside renderPokemonCard: ", pokemon)
+
+  const containerEl = document.createElement("li")
+  containerEl.className = "card"
+
+  const headingEl = document.createElement("h2")
+  headingEl.className = "card--title"
+
+  headingEl.innerText = pokemon.name.toUpperCase()
+
+  containerEl.append(headingEl)
+
+  const imageEl = document.createElement("img")
+
+  const imageSrc = pokemon.sprites.other["official-artwork"].front_default
+
+  imageEl.setAttribute("width", "256")
+  imageEl.setAttribute("class", "card--img")
+  imageEl.setAttribute("src", imageSrc)
+
+  containerEl.append(imageEl)
+
+  const listEl = document.createElement("ul")
+  listEl.className = "card--text"
+
+  for (let i = 0; i < pokemon.stats.length; i++) {
+    const statItem = pokemon.stats[i]
+    const baseStat = statItem.base_stat
+    const statName = statItem.stat.name
+
+    const listItemEl = document.createElement("li")
+
+    listItemEl.innerText = `${statName.toUpperCase()}: ${baseStat}`
+
+    listEl.append(listItemEl)
+  }
+
+  containerEl.append(listEl)
+
+  return containerEl
 }
-const pokemonCards = renderPokemon(data)
-// console.log("pokemonCards: ", pokemonCards)
-function renderStatsList() {
-    let pokemonStats = {}
-    for (let i = 0; i < data.length; i++) {
-        const item = data[i]
-        const pokemonStats = item.stats
-console.log("Pokemon Stats: ", pokemonStats)
-    }
-    return pokemonStats
+
+for (let i = 0; i < data.length; i++) {
+  const pokemon = data[i]
+
+  // console.log("Before renderPokemonCard: ", pokemon)
+
+  const pokemonCard = renderPokemonCard(pokemon)
+
+  listEl.append(pokemonCard)
 }
-const pokemonRenderStats = renderPokemon(data)
-// console.log("pokemonRenderStats: ", pokemonRenderStats)
